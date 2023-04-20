3149ff39a08598cb45dca876b034565a1bc62af7
Ruiyan Liang <Jliang@lcs.on.ca>
2023-04-20 09:49:44 -0400

    Initial Commit

---
+++ BookFinder/BookFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookFinderApp.swift
+//  BookFinder
+//
+//  Created by dyhidrogen monoxide on 2023/4/20.
+//
+
+import SwiftUI
+
+@main
+struct BookFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
